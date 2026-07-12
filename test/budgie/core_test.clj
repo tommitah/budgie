f5a5c5399905cb45c4d24fbfc74c434626e3cca5
tommi-tahvanainen_ACC <tommi.tahvanainen@finago.com>
2026-07-12 11:32:32 +0300

    test-runner

--- test/budgie/core_test.clj
+++ test/budgie/core_test.clj
@@ -4,6 +4,13 @@
     [clojure.test :refer [deftest testing is]]))
 
 
-(deftest test-add
+(deftest add-test
   (testing "1 + 2 = 3"
     (is (= 3 (add 1 2)))))
+
+
+(deftest multiple-param-test
+  (testing "Testing n-amount of params"
+    (and (is (= 3 (add 1 1 1)))
+         (is (= 4 (add 2 2 0)))
+         (is (= 9 (add 2 1 1 1 2 2))))))
