2f95e5c6f764cab6459bdb524531c71c7c15d4f7
tommi-tahvanainen_ACC <tommi.tahvanainen@finago.com>
2026-07-12 17:07:36 +0300

    `file`: file storage write test

--- src/budgie/file.clj
+++ src/budgie/file.clj
@@ -6,15 +6,20 @@
 
 (defn -ensure-dir! [path] (fs/create-dirs path))
 
+(defn -entry-file-path [entry] (fs/path default-path (str (:id entry) ".edn")))
+
 (defn write-entry!
   "Writes budget entry contents into a file named via the uuid of the entry data.\n
   Ensures the file directory exists on the local machine.\n
   Returns file path."
   [entry]
   (-ensure-dir! default-path)
-  (let [path (fs/path default-path (str (:id entry) ".edn"))]
+  (let [path (-entry-file-path entry)]
+    ;; write the entry edn contents ar a str in path
     (spit (str path) (pr-str entry))
     path))
 
 (comment
-  (write-entry! {:id "some_id" :foo "bar"}))
+  (write-entry! {:id "some_id" :foo "bar"})
+  (pr-str {:id "some-id" :foo :bar})
+  (str (fs/path default-path (str (:id {:id "some-id" :foo :bar}) ".edn"))))
---
+++ test/budgie/file_test.clj
@@ -0,0 +1,28 @@
+(ns budgie.file-test
+  (:require [budgie.file :as m]
+            [babashka.fs :as fs]
+            [clojure.edn :as edn]
+            [clojure.test :refer [deftest is testing use-fixtures]]))
+
+(def test-path (atom nil))
+(def test-id "some-id")
+(def test-entry {:id test-id :some :data})
+
+(defn clean-test-files!
+  []
+  (when (and @test-path (fs/exists? @test-path)) (fs/delete-tree @test-path)))
+
+(defn with-test-path
+  [test-fn]
+  (reset! test-path (fs/create-temp-dir {:prefix "budgie-test-"}))
+  (try (test-fn) (finally (clean-test-files!) (reset! test-path nil))))
+
+(use-fixtures :each with-test-path)
+
+(deftest write-entry!-test
+  (testing "Writing edn-file entry from entry data"
+    (with-redefs [m/default-path @test-path]
+      (let [expected-path (fs/path @test-path (str test-id ".edn"))
+            actual-path (m/write-entry! test-entry)]
+        (is (= (str expected-path) (str actual-path)))
+        (is (= test-entry (edn/read-string (slurp (str actual-path)))))))))
