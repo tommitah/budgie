6af2e2ef082048fb0ee4bd432987d90cdcfc35d2
tommi-tahvanainen_ACC <tommi.tahvanainen@finago.com>
2026-07-12 13:19:50 +0300

    private the main entry point

--- src/budgie/core.clj
+++ src/budgie/core.clj
@@ -1,16 +1,17 @@
 (ns budgie.core
-  (:require [budgie.entry :as budget-entry]))
+  (:require [budgie.entry :as budget-entry]
+            [clojure.pprint :as pp]))
 
 (defn add "Naively adds two numbers together" [x & xs] (apply + x xs))
 
-(defn main
+(defn -main
   [opts]
   ;; todo: spec out the cli opts in `cli_command`
   (println "Calling with options: " opts)
   (let [{:keys [category amount]} opts
         entry (budget-entry/create category amount)]
-    (println "Created entry:")
-    (println entry)
+    (pp/pprint "Created entry:")
+    (pp/pprint entry)
     ;; todo: save the entry in a file (new file per entry, named via uuid)
   ))
 
--- test/budgie/core_test.clj
+++ test/budgie/core_test.clj
@@ -5,7 +5,7 @@
 (deftest add-test (testing "1 + 2 = 3" (is (= 3 (m/add 1 2)))))
 
 (deftest multiple-param-add-test
-  (testing "Testing n-amount of params"
+  (testing "add n-amount of params"
     (and (is (= 3 (m/add 1 1 1)))
          (is (= 4 (m/add 2 2 0)))
          (is (= 9 (m/add 2 1 1 1 2 2))))))
